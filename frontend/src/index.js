9a628d1b7b18c7767dbe35b0d0aee03ab9401e53
Tyler <39608585+TylerSowers@users.noreply.github.com>
2023-12-13 20:54:32 -0500

    reactdomfixes build changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
+ReactDOM.render(
+ <BrowserRouter>
     <App />
-  </BrowserRouter>
+ </BrowserRouter>,
+ document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
