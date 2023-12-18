af38ace5c6bd9f2f27405d9eddbd973e6cd87c0d
Tyler <39608585+TylerSowers@users.noreply.github.com>
2023-12-18 15:32:49 -0500

    implimented pagination for the backend on the/cancelled route

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,47 +1,73 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Post = require('../models/postModel');
-const Cancelled = require('../models/cancelledModel');
-const Transcript = require('../models/transcriptModel');
+const Post = require("../models/postModel");
+const Cancelled = require("../models/cancelledModel");
+const Transcript = require("../models/transcriptModel");
 
+const ITEMS_PER_PAGE = 50; // Adjust the number of items per page as needed
 
+router.get("/cancelled", async function (req, res, next) {
+  try {
+    const page = parseInt(req.query.page) || 1;
 
-router.get('/posts',function(req,res,next){
-    Post.find({}).then(function(posts){
-        res.send(posts);
-    }).catch(next);
+    const cancel = await Cancelled.find({})
+      .sort({ episode: 1 }) // Sort by the "episode" field in ascending order
+      .skip((page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE);
+
+    res.send(cancel);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/cancelled',function(req,res,next){
-    Cancelled.find({}).then(function(cancel){
-        res.send(cancel);
-    }).catch(next);
+router.get("/cancelled/meta", async function (req, res, next) {
+  try {
+    const totalCount = await Cancelled.countDocuments({});
+    res.json({ totalCount });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/cancelled',function(req,res,next){
-    Cancelled.create(req.body).then(function(cancel){
-        res.send(cancel);
-    }).catch(next);
+router.get("/posts", async function (req, res, next) {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const skippedItems = (page - 1) * ITEMS_PER_PAGE;
+
+    const posts = await Post.find({}).skip(skippedItems).limit(ITEMS_PER_PAGE);
+
+    res.send(posts);
+  } catch (error) {
+    next(error);
+  }
 });
 
-
-router.post('/posts',function(req,res,next){
-    Post.create(req.body).then(function(posts){
-        res.send(posts);
-    }).catch(next);
+router.post("/cancelled", async function (req, res, next) {
+  try {
+    const cancel = await Cancelled.create(req.body);
+    res.send(cancel);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/transcripts',function(req,res,next){
-    Transcript.create(req.body).then(function(transcript){
-        res.send(transcript);
-    }).catch(next);
+router.post("/posts", async function (req, res, next) {
+  try {
+    const posts = await Post.create(req.body);
+    res.send(posts);
+  } catch (error) {
+    next(error);
+  }
 });
 
+router.post("/transcripts", async function (req, res, next) {
+  try {
+    const transcript = await Transcript.create(req.body);
+    res.send(transcript);
+  } catch (error) {
+    next(error);
+  }
+});
 
-// router.delete('/posts',function(req,res,next){
-//     Post.create(req.body).then(function(posts){
-//         res.send(posts);
-//     }).catch(next);
-// });
-
-module.exports = router
+module.exports = router;
--- frontend/src/components/transcript.jsx
+++ frontend/src/components/transcript.jsx
@@ -1,36 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { Card, CardContent, Typography, TextField, Box, Grid, Button, IconButton } from '@material-ui/core';
-import { ExpandMore as ExpandMoreIcon, ExpandLess as ExpandLessIcon } from '@material-ui/icons';
-import axios from 'axios';
-import { makeStyles } from '@material-ui/core/styles';
-import { Dialog, DialogTitle, DialogContent, DialogActions } from '@material-ui/core';
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  Box,
+  Grid,
+  Button,
+  IconButton
+} from "@material-ui/core";
+import {
+  ExpandMore as ExpandMoreIcon,
+  ExpandLess as ExpandLessIcon
+} from "@material-ui/icons";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   episodeText: {
-    color: '#cb767d',
-  },
+    color: "#cb767d"
+  }
 }));
 const TranscriptCard = () => {
   const [transcript, setTranscript] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [expandedDialogCardId, setExpandedDialogCardId] = useState(null);
-  const [expandedTranscript, setExpandedTranscript] = useState('');
+  const [expandedTranscript, setExpandedTranscript] = useState("");
   const [expandedCardId, setExpandedCardId] = useState(null);
-  const [currentEpisodeTitle, setCurrentEpisodeTitle] = useState('');
-
+  const [currentEpisodeTitle, setCurrentEpisodeTitle] = useState("");
 
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 100;
+  const itemsPerPage = 50;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:9000/api/posts');
-        const sortedTranscript = response.data.sort((a, b) => a.episode - b.episode);
+        const response = await axios.get("http://localhost:9000/api/posts");
+        const sortedTranscript = response.data.sort(
+          (a, b) => a.episode - b.episode
+        );
         setTranscript(sortedTranscript);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -39,15 +57,18 @@ const TranscriptCard = () => {
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
-    setCurrentPage(1); 
+    setCurrentPage(1);
   };
 
   const highlightText = (text) => {
-    const regex = new RegExp(searchQuery, 'gi');
-    return text.replace(regex, (match) => `<span style="background-color: yellow">${match}</span>`);
+    const regex = new RegExp(searchQuery, "gi");
+    return text.replace(
+      regex,
+      (match) => `<span style="background-color: yellow">${match}</span>`
+    );
   };
 
-   const openTranscriptDialog = (cardId) => {
+  const openTranscriptDialog = (cardId) => {
     const transcriptItem = transcript.find((item) => item._id === cardId);
     if (transcriptItem) {
       setExpandedTranscript(transcriptItem.transcript);
@@ -71,7 +92,10 @@ const TranscriptCard = () => {
 
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = filteredTranscripts.slice(indexOfFirstItem, indexOfLastItem);
+  const currentItems = filteredTranscripts.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
   const totalPages = Math.ceil(filteredTranscripts.length / itemsPerPage);
 
   const handlePageChange = (pageNumber) => {
@@ -82,38 +106,62 @@ const TranscriptCard = () => {
   return (
     <div>
       <Box display="flex" justifyContent="center" marginTop={2}>
-        {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNumber) => (
-          <button key={pageNumber} onClick={() => handlePageChange(pageNumber)}>
-            {pageNumber}
-          </button>
-        ))}
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map(
+          (pageNumber) => (
+            <button
+              key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
+            >
+              {pageNumber}
+            </button>
+          )
+        )}
       </Box>
       <Box display="flex" justifyContent="center" marginBottom={2}>
-        <TextField label="Search Transcript" value={searchQuery} onChange={handleSearchChange} />
+        <TextField
+          label="Search Transcript"
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
       </Box>
       <Grid container spacing={2}>
         {currentItems.map((transcriptItem) => (
           <Grid item xs={12} sm={6} key={transcriptItem._id}>
             <Card>
               <CardContent>
-                <Box display="flex" alignItems="center" justifyContent="space-between">
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                >
                   <Typography variant="h5" component="h2">
                     {transcriptItem.title}
                   </Typography>
-                  <Button onClick={() => openTranscriptDialog(transcriptItem._id)}>Popout</Button>
-                  <IconButton onClick={() => toggleCardExpansion(transcriptItem._id)}>
-                    {expandedCardId === transcriptItem._id ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                  <Button
+                    onClick={() => openTranscriptDialog(transcriptItem._id)}
+                  >
+                    Popout
+                  </Button>
+                  <IconButton
+                    onClick={() => toggleCardExpansion(transcriptItem._id)}
+                  >
+                    {expandedCardId === transcriptItem._id ? (
+                      <ExpandLessIcon />
+                    ) : (
+                      <ExpandMoreIcon />
+                    )}
                   </IconButton>
                 </Box>
                 <Typography color="textSecondary" gutterBottom>
-                  <span className={classes.episodeText}>Episode:</span> {transcriptItem.episode}
+                  <span className={classes.episodeText}>Episode:</span>{" "}
+                  {transcriptItem.episode}
                 </Typography>
                 {expandedCardId === transcriptItem._id && (
                   <Typography
                     variant="body2"
                     component="p"
                     dangerouslySetInnerHTML={{
-                      __html: highlightText(transcriptItem.transcript),
+                      __html: highlightText(transcriptItem.transcript)
                     }}
                   />
                 )}
@@ -127,13 +175,13 @@ const TranscriptCard = () => {
         open={isDialogOpen}
         onClose={() => setIsDialogOpen(false)}
         maxWidth="lg"
-        >
-      <DialogTitle>{currentEpisodeTitle}</DialogTitle>
+      >
+        <DialogTitle>{currentEpisodeTitle}</DialogTitle>
         <DialogContent>
           <Typography variant="body2" component="p">
             <span
               dangerouslySetInnerHTML={{
-                __html: highlightText(expandedTranscript),
+                __html: highlightText(expandedTranscript)
               }}
               className={classes.highlightedText}
             />
@@ -146,11 +194,16 @@ const TranscriptCard = () => {
         </DialogActions>
       </Dialog>
       <Box display="flex" justifyContent="center" marginTop={2}>
-        {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNumber) => (
-          <button key={pageNumber} onClick={() => handlePageChange(pageNumber)}>
-            {pageNumber}
-          </button>
-        ))}
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map(
+          (pageNumber) => (
+            <button
+              key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
+            >
+              {pageNumber}
+            </button>
+          )
+        )}
       </Box>
     </div>
   );
